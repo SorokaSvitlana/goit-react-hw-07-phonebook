d03b3f0776ae168ab5eb5e32c22e4d8192256ebd
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-06-18 13:12:43 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import React, { useEffect }  from "react";
+import { Form } from "./FormAddContacts/FormAddContacts";
+import { ContactList } from "./Contacts/Contacts";
+import { Search } from "./Search/Search";
+import { Container, Title, ListTitle } from './Container/Container.Styled'
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/operation";
+
+export function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Title>Phonebook</Title>
+      <Form />
+      <ListTitle>Contacts</ListTitle>
+      <Search />
+      <ContactList  />
+    </Container>
   );
-};
+}
